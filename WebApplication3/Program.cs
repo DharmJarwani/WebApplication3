d13b7707cf24f647781877e558a36f0e9860dc75
DharmJarwani <dharmendhar.om@gmail.com>
2024-03-23 12:38:52 +0530

    Facing problem in adding swagger

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using WebApplication3.Data;
 using WebApplication3.Repositories;
 using WebApplication3.Services;
@@ -14,11 +15,32 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 
+// Add Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Name", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name v1");
+    });
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
 
+app.UseRouting();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
